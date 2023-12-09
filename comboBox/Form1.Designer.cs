6c0079c981e51cc1e025c6e37efc4701b7ea38ba
kayoGc <kayivciti@gmail.com>
2023-12-09 01:13:05 -0300

    Add project files.

---
+++ comboBox/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace comboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbEstados = new System.Windows.Forms.ComboBox();
+            this.lblDigite = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbEstados
+            // 
+            this.cmbEstados.FormattingEnabled = true;
+            this.cmbEstados.Location = new System.Drawing.Point(427, 126);
+            this.cmbEstados.Name = "cmbEstados";
+            this.cmbEstados.Size = new System.Drawing.Size(121, 21);
+            this.cmbEstados.TabIndex = 0;
+            this.cmbEstados.SelectedIndexChanged += new System.EventHandler(this.cmbEstados_SelectedIndexChanged);
+            // 
+            // lblDigite
+            // 
+            this.lblDigite.AutoSize = true;
+            this.lblDigite.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDigite.Location = new System.Drawing.Point(71, 117);
+            this.lblDigite.Name = "lblDigite";
+            this.lblDigite.Size = new System.Drawing.Size(350, 39);
+            this.lblDigite.TabIndex = 1;
+            this.lblDigite.Text = "Selecione um estado:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDigite);
+            this.Controls.Add(this.cmbEstados);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbEstados;
+        private System.Windows.Forms.Label lblDigite;
+    }
+}
+
---
+++ comboBox/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace comboBox
+{
+    public partial class Form1 : Form
+    {
+        List<Estado> estados = new List<Estado>();
+        public Form1()
+        {
+            InitializeComponent();
+            PopularEstados();
+            InicializarCampos();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbEstados_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void InicializarCampos()
+        {
+            cmbEstados.DisplayMember = "Nome";
+            cmbEstados.ValueMember = "Sigla";
+            cmbEstados.DataSource = estados;
+        }
+
+        public void PopularEstados()
+        {
+            estados.Add(new Estado("SP", "São Paulo"));
+            estados.Add(new Estado("ES", "Espirito Santo"));
+            estados.Add(new Estado("MG", "Minas Gerais"));
+            estados.Add(new Estado("RJ", "Rio de Janeiro"));
+            estados.Add(new Estado("PR", "Paraná"));
+            estados.Add(new Estado("RS", "Rio Grande do Sul"));
+            estados.Add(new Estado("SC", "Santa Catarina"));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
